e178cf7276fa1cc90714e20cfe8afffd6a318b30
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2022-06-30 01:47:09 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,10 +66,10 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
     
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/dawn_in")
+    buttons.buildbutton("Uncensored Playground ", "https://t.me/UncensoredPlaygrounds")
+    
+    
+    
     
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
@@ -175,7 +175,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Z-Mirror-Bot Help',
+        title='Uncensored-Playground-help',
         content=help_string_telegraph,
     )["path"]
 
